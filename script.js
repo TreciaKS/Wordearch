5afa4eddfe6d8ffd09bee06040dd1cc10e88e12c
Trecia Kat <tksekgalo@gmail.com>
2022-02-27 20:27:45 +0200

    styled the rest of the results, need to modify things

--- script.js
+++ script.js
@@ -11,44 +11,47 @@ btn.addEventListener("click", () => {
 
             // display results in the DOM
             result.innerHTML = `
-                <div class="word">
+                <div class="text-xl text-center text-indigo-500 text-decoration-underline pt-2 pb-2">
                     <h3>${inpWord}</h3>
                 </div>
 
-                <div class="py-8 flex flex-wrap md:flex-nowrap">
+                <div class="py-8 flex flex-wrap md:flex-nowrap shadow-lg">
                     <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                        <span class="font-semibold title-font text-gray-700">${data[0].meanings[0].partOfSpeech}</span>
+                        <span class="font-semibold title-font text-indigo-500">${data[0].meanings[0].partOfSpeech}</span>
                     </div>
-                    <div class="md:flex-grow">
+                    <div class="md:flex-grow text-gray-200">
                         <p class="leading-relaxed">1. ${data[0].meanings[0].definitions[0].definition || ""}</p>
                         <p class="mt-5 leading-relaxed">2. ${data[0].meanings[0].definitions[1].definition || ""}</p>
                         <p class="mt-5 leading-relaxed">3. ${data[0].meanings[0].definitions[2].definition || ""}</p>
                     </div>
                 </div>
                 
-                <div class="secondDetails">
-                    <p>${data[0].meanings[1].partOfSpeech}</p>
-
-                <div class="word-meaning">
-                    <p>1. ${data[0].meanings[1].definitions[0].definition || ""}</p>
-                    <p>2. ${data[0].meanings[1].definitions[1].definition || ""}</p>
-                    <p>3. ${data[0].meanings[1].definitions[2].definition || ""}</p>
+                <div class="py-8 flex flex-wrap md:flex-nowrap shadow-lg">
+                    <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                        <span class="font-semibold title-font text-indigo-500">${data[0].meanings[1].partOfSpeech}</span>
+                    </div>
+                    <div class="md:flex-grow text-gray-200">
+                        <p class="leading-relaxed">1. ${data[0].meanings[1].definitions[0].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">2. ${data[0].meanings[1].definitions[1].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">3. ${data[0].meanings[1].definitions[2].definition || ""}</p>
+                    </div>
                 </div>
 
-                </div>
-
-                <p class="word-example">
-                    ${data[0].meanings[0].definitions[0].example || ""}
-                </p>`;
+                <div class="py-8 flex flex-wrap md:flex-nowrap shadow-lg">
+                    <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                        <span class="font-semibold title-font text-indigo-500">Example</span>
+                    </div>
+                    <div class="md:flex-grow text-gray-200">
+                        <p class="leading-relaxed">1. ${data[0].meanings[0].definitions[0].example || ""}</p>
+                    </div>
+                </div>`
         })
 
         // error handling
         .catch(() => {
-            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+            result.innerHTML = `<h3 class="error text-center text-2xl text-gray-500 mt-10">I can't access the word 🥺</h3>`;
         });
-});
 
-// audio sound for dictionary words
-function playSound() {
-    sound.play();
-}
+        // clear out form
+        inpWord.innerHTML = ""
+});
