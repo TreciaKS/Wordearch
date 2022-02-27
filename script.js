f84b11676fbfc5e4ced973d4a3cd022a6b3c717a
Trecia Kat <tksekgalo@gmail.com>
2022-02-27 16:30:05 +0200

    removed code that had to do with the audio id

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
-const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", () => {
@@ -12,11 +11,8 @@ btn.addEventListener("click", () => {
 
             // display results in the DOM
             result.innerHTML = `
-            <div class="word">
+                <div class="word">
                     <h3>${inpWord}</h3>
-                    <button onclick="playSound()">
-                        <i class="fas fa-volume-up"></i>
-                    </button>
                 </div>
                 <div class="details">
                     <p>${data[0].meanings[0].partOfSpeech}</p>
@@ -28,7 +24,6 @@ btn.addEventListener("click", () => {
                 <p class="word-example">
                     ${data[0].meanings[0].definitions[0].example || ""}
                 </p>`;
-            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
         })
 
         // error handling
