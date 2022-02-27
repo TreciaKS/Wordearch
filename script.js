1a67278dbf9de099bd77c24d6d7e52f4d98c79cf
Trecia Kat <tksekgalo@gmail.com>
2022-02-27 19:01:39 +0200

    styled first results and yes, they work

--- script.js
+++ script.js
@@ -14,15 +14,18 @@ btn.addEventListener("click", () => {
                 <div class="word">
                     <h3>${inpWord}</h3>
                 </div>
-                <div class="firstDetails">
-                    <p>${data[0].meanings[0].partOfSpeech}</p>
 
-                <div class="word-meaning text-red-500">
-                    <p>1. ${data[0].meanings[0].definitions[0].definition || ""}</p>
-                    <p>2. ${data[0].meanings[0].definitions[1].definition || ""}</p>
-                    <p>3. ${data[0].meanings[0].definitions[2].definition || ""}</p>
+                <div class="py-8 flex flex-wrap md:flex-nowrap">
+                    <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                        <span class="font-semibold title-font text-gray-700">${data[0].meanings[0].partOfSpeech}</span>
+                    </div>
+                    <div class="md:flex-grow">
+                        <p class="leading-relaxed">1. ${data[0].meanings[0].definitions[0].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">2. ${data[0].meanings[0].definitions[1].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">3. ${data[0].meanings[0].definitions[2].definition || ""}</p>
+                    </div>
                 </div>
-
+                
                 <div class="secondDetails">
                     <p>${data[0].meanings[1].partOfSpeech}</p>
 
