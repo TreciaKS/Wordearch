345a2ffb8f76fc065b03f0339a2dae67a761a4d6
Trecia Kat <tksekgalo@gmail.com>
2022-02-28 11:38:54 +0200

    removed code that is causing the bug

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const result = document.getElementById("result");
 const btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", () => {
-    let inpWord = document.getElementById("inp-word").value;
+    let inpWord = document.getElementById("inp-word").value
     fetch(`${url}${inpWord}`)
         .then((response) => response.json())
         .then((data) => {
@@ -11,11 +11,11 @@ btn.addEventListener("click", () => {
 
             // display results in the DOM
             result.innerHTML = `
-                <div class="text-xl text-center text-indigo-500 text-decoration-underline pt-2 pb-2">
+                <div class="text-3xl text-center text-indigo-500 capitalize underline pt-2 pb-2">
                     <h3>${inpWord}</h3>
                 </div>
 
-                <div class="py-8 flex flex-wrap md:flex-nowrap shadow-lg">
+                <div class="py-8 flex flex-wrap md:flex-nowrap">
                     <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                         <span class="font-semibold title-font text-indigo-500">${data[0].meanings[0].partOfSpeech}</span>
                     </div>
@@ -23,26 +23,18 @@ btn.addEventListener("click", () => {
                         <p class="leading-relaxed">1. ${data[0].meanings[0].definitions[0].definition || ""}</p>
                         <p class="mt-5 leading-relaxed">2. ${data[0].meanings[0].definitions[1].definition || ""}</p>
                         <p class="mt-5 leading-relaxed">3. ${data[0].meanings[0].definitions[2].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">4. ${data[0].meanings[0].definitions[3].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">5. ${data[0].meanings[0].definitions[4].definition || ""}</p>
                     </div>
                 </div>
-                
-                <div class="py-8 flex flex-wrap md:flex-nowrap shadow-lg">
-                    <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                        <span class="font-semibold title-font text-indigo-500">${data[0].meanings[1].partOfSpeech}</span>
-                    </div>
-                    <div class="md:flex-grow text-gray-200">
-                        <p class="leading-relaxed">1. ${data[0].meanings[1].definitions[0].definition || ""}</p>
-                        <p class="mt-5 leading-relaxed">2. ${data[0].meanings[1].definitions[1].definition || ""}</p>
-                        <p class="mt-5 leading-relaxed">3. ${data[0].meanings[1].definitions[2].definition || ""}</p>
-                    </div>
-                </div>
-
-                <div class="py-8 flex flex-wrap md:flex-nowrap shadow-lg">
+               
+                <hr class="border-solid border-1 border-indigo-500">
+                <div class="py-8 flex flex-wrap md:flex-nowrap">
                     <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                         <span class="font-semibold title-font text-indigo-500">Example</span>
                     </div>
                     <div class="md:flex-grow text-gray-200">
-                        <p class="leading-relaxed">1. ${data[0].meanings[0].definitions[0].example || ""}</p>
+                        <p class="leading-relaxed">1. ${data[0].meanings[0].definitions[0].example || "Not available"}</p>
                     </div>
                 </div>`
         })
@@ -51,7 +43,4 @@ btn.addEventListener("click", () => {
         .catch(() => {
             result.innerHTML = `<h3 class="error text-center text-2xl text-gray-500 mt-10">I can't access the word 🥺</h3>`;
         });
-
-        // clear out form
-        inpWord.innerHTML = ""
 });
